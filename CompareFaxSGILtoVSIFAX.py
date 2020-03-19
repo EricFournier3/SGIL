ef619bda58e9edc87f299756bef6fe85e56f29ce
Éric Fournier <foueri01@inspq.qc.ca@slbio00d.inspq.qc.ca>
2020-03-19 11:11:51 -0400

    Eric FOurnier 2020-03-19

---
+++ CompareFaxSGILtoVSIFAX.py
@@ -0,0 +1,54 @@
+import os
+import re
+
+fax_file_path = os.path.join("/data/Applications/GitScript/SGIL/FAX_STATUS_20200318_08H_26m.txt")
+sgil_file_path = os.path.join("/data/Applications/GitScript/SGIL/export_20200318.txt")
+
+envois_sgil = []
+envois_fax = []
+
+
+with open(fax_file_path) as fax_f:
+    fax_f.readline()
+    for line in fax_f:
+        try:
+            l = line.split('Rapport lab')[1]
+            #print l
+            envoi = re.search('#Env 00(\d+) ',l).group(1)
+            #print "Envoi ",envoi
+            envois_fax.append(envoi)
+        except:
+            print "Error ",line
+
+
+envois_fax = set(envois_fax)
+#print "FAX = ", envois_fax
+print "Nb VSIFAX ", len(envois_fax)
+
+
+
+
+with open(sgil_file_path) as sgil_f:
+    sgil_f.readline()
+    for line in sgil_f:
+        try:
+            l = line.split('\t')
+            envoi = l[4]
+            #envoi = re.search('#Env 00(\d+) ',l).group(1)
+
+            envois_sgil.append(envoi)
+        except:
+            print "Error ",line
+
+envois_sgil = set(envois_sgil)
+#print "SGIL = ", envois_sgil, "   "
+print "Nb SGIL ", len(envois_sgil)
+
+
+
+in_fax_notin_sgil = envois_fax - envois_sgil
+print "In VSIFAX NOT IN SGIL ", in_fax_notin_sgil
+
+in_sgil_notin_fax = envois_sgil - envois_fax
+print "In SGIL NOT IN VSIFAX ", in_sgil_notin_fax
+
